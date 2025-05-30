f924d726971211780729458b204879b4a5ab03ec
cedricrgt <128705301+cedricrgt@users.noreply.github.com>
2025-05-30 10:22:13 +0200

    Resolve merge conflicts in vite.config.ts
    # Conflicts:
    #	.gitignore
    #	src/assets/fonts/League_Spartan/OFL.txt

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: "/RHsummerSession/",
->>>>>>> b8f125d (Initial commit)
 });
